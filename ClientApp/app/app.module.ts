6c76bd384610b5f330cea4a45d08b76702ef010d
Elizabeth Connolly <Elizabeth.Connolly@insight.com>
2017-04-27 11:06:32 -0400

    initial test component and web compiler files

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -3,12 +3,14 @@ import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        HeaderComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
